9cae6baa0d5e26cd824b89323ac324c62a980eb2
r0987765 <r0987765@gmail.com>
2024-05-26 04:04:43 +0800

    upg

--- work2.py
+++ work2.py
@@ -1,4 +1,6 @@
+# 使用者輸入
+inp = input('輸入年月日(ex:0990301)')
+
 import requests,bs4
-response = requests.get('https://www.taiwanlottery.com/')
+response = requests.get('https://www.taiwanlottery.com/lotto/history/history_result')
 obj = bs4.BeautifulSoup(response.text,'lxml')
-print(obj)
