3841cf254d215779fd5ca181058e1ef17afa506b
r0987765 <r0987765@gmail.com>
2024-05-26 21:00:23 +0800

    upg

--- work2.py
+++ work2.py
@@ -1,6 +1,29 @@
+# 查詢開獎號碼並自動開啟瀏覽器搜尋該期別方便對照
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time , requests
 # 使用者輸入
-inp = input('輸入年月日(ex:0990301)')
-
-import requests,bs4
-response = requests.get('https://www.taiwanlottery.com/lotto/history/history_result')
-obj = bs4.BeautifulSoup(response.text,'lxml')
+year = input('西元年分')
+month = input('月(ex : 5月 => 05)')
+day = input('日(ex : 5日 => 05)')
+browser = webdriver.Chrome()
+# 擷取網頁
+url = 'https://api.taiwanlottery.com/TLCAPIWeB/Lottery/SuperLotto638Result?period&month='+year+'-'+month+'&pageNum=1&pageSize=10'
+url2 = 'https://www.taiwanlottery.com/lotto/history/history_result'
+html = requests.get(url)
+browser.get(url2)
+# 轉換json檔案
+data = html.json()
+# 查找元素
+datalist = data['content']['superLotto638Res']
+lottery = year+'-'+month+'-'+day+'T00:00:00'
+for i in range(len(datalist)):
+    if datalist[i]['lotteryDate'] == lottery:
+        period = datalist[i]['period']
+        num = datalist[i]['drawNumberSize']
+        print('開獎號碼 : ',end='')
+        print(*num)
+# 自動輸入開獎期別
+browser.find_element(By.ID,'el-id-1024-8').send_keys(period)
+browser.find_element(By.XPATH,'//*[@id="__nuxt"]/main/div[1]/div[2]/div/button').click()
+time.sleep(10)
